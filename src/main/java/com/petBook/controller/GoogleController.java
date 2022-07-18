8683894b9d0d463eca30a34329c850cb44be401e
mjk6828 <mjk6829@gmail.com>
2022-07-18 22:17:01 +0900

    google login update

--- src/main/java/com/petBook/controller/GoogleController.java
+++ src/main/java/com/petBook/controller/GoogleController.java
@@ -19,6 +19,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
@@ -40,7 +42,8 @@ public class GoogleController {
     public GoogleController(ConfigUtils configUtils) {
         this.configUtils = configUtils;
     }
-//    @Tag(name="소셜 로그인", description = "Google 소셜 로그인 API")
+
+    //    @Tag(name="소셜 로그인", description = "Google 소셜 로그인 API")
     @ApiOperation(
             value = "Google 로그인",
             notes = "Google 로그인 페이지로 이동하는 메소드"
@@ -51,9 +54,9 @@ public class GoogleController {
                             responseCode = "200",
                             description = "토큰 발급 성공",
                             content = @Content(
-                            schema = @Schema(
-                                    implementation = GoogleLoginResponse.class
-                    )))
+                                    schema = @Schema(
+                                            implementation = GoogleLoginResponse.class
+                                    )))
             })
     @GetMapping(value = "/google/login")
     public ResponseEntity<GoogleLoginResponse> moveGoogleInitUrl() {
@@ -70,57 +73,45 @@ public class GoogleController {
         return ResponseEntity.badRequest().build();
     }
 
-//    @Tag(name="소셜 로그인", description = "Google 소셜 로그인 API")
+    //    @Tag(name="소셜 로그인", description = "Google 소셜 로그인 API")
     @GetMapping(value = "/google/login/oauth2")
     public ResponseEntity<Object> redirectGoogleLogin(
             @RequestParam(value = "code") String authCode
     ) {
-        log.info("code : "+ authCode);
+        log.info("code : " + authCode);
         //HTTP 통신을 위해 RestTemplate 활용
         RestTemplate restTemplate = new RestTemplate();
-        GoogleLoginRequest requestParams = GoogleLoginRequest.builder()
-                .clientId(configUtils.getGoogleClientId())
-                .clientSecret(configUtils.getGoogleSecret())
-                .code(authCode)
-                .redirectUri(configUtils.getGoogleRedirectUrl())
-                .grantType("authorization_code")
-                .build();
-
         try {
+            MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+            params.add("client_id", configUtils.getGoogleClientId());
+            params.add("client_secret", configUtils.getGoogleSecret());
+            params.add("code", authCode);
+            params.add("redirect_uri", configUtils.getGoogleRedirectUrl());
+            params.add("grant_type", "authorization_code");
             // Http Header 설정
             HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            HttpEntity<GoogleLoginRequest> httpRequestEntity = new HttpEntity<>(requestParams, headers);
-            ResponseEntity<String> apiResponseJson = restTemplate.postForEntity(configUtils.getGoogleAuthUrl()
-                    + "/token", httpRequestEntity, String.class);
+//            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.set("Content-Type", MediaType.APPLICATION_FORM_URLENCODED_VALUE);
+            HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(params, headers);
 
-            // ObjevtMapper를 통해 String to Objet로 변환
+            ResponseEntity<String> response = restTemplate.exchange(
+                    configUtils.getGoogleAuthUrl() + "/token",
+                    HttpMethod.POST,
+                    entity,
+                    String.class
+            );
+            log.info("response: " + response);
+
             ObjectMapper objectMapper = new ObjectMapper();
             objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
-            objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL); //NULL 이 아닌 값만 응답받기(Null인 경우는 생략)
-            GoogleLoginResponse googleLoginResponse = objectMapper.readValue(apiResponseJson.getBody(),
-                    new TypeReference<GoogleLoginResponse>() {});
+            objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+            GoogleLoginResponse googleLoginResponse = objectMapper.readValue(response.getBody(),
+                    new TypeReference<GoogleLoginResponse>() {
+                    });
 
-            // 사용자의 정보는 JWT Token 으로 저장되어 있꼬, Id_Token에 값을 저장한다.
-            String jwtToken = googleLoginResponse.getIdToken();
+            return ResponseEntity.ok().body(googleLoginResponse);
 
-            // JWT Token을 전달해 JWT 저장된 사용자 정보 확인
-            String requestUrl = UriComponentsBuilder.fromHttpUrl(configUtils.getGoogleAuthUrl() + "/tokeninfo").queryParam("id_token", jwtToken).toUriString();
-
-            String resultJson = restTemplate.getForObject(requestUrl, String.class);
-
-            if(resultJson != null){
-                GoogleLoginVO googleLoginVO = objectMapper.readValue(resultJson, new TypeReference<GoogleLoginVO>() {});
-
-                Map<String, String> result = new HashMap<>();
-                result.put("token", jwtToken);
-
-                return ResponseEntity.ok().body(googleLoginResponse);
-            }else{
-                throw new Exception("Google OAuth failed!");
-            }
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
--- src/main/java/com/petBook/controller/TestController.java
+++ src/main/java/com/petBook/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.petBook.controller;
 
+import com.petBook.vo.GoogleLoginVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
